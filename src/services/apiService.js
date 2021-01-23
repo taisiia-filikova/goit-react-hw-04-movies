b10e5bcd74d57480c9ffa598ab04d39d2289b320
taisiia-filikova <filikova@gmail.com>
2021-01-23 16:19:23 +0200

    Подключает API

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,11 +1,35 @@
-import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '2a1f6a16e70b08ad0ce6b62ba69400fc';
 
-const apiService = async (query, page) => {
-  const API_KEY = '19086607-f63ea5bac9dbca697b9469f63';
-  const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+async function apiService(url = '', config = {}) {
+  const response = await fetch(url, config);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error('Sorry, we found nothing on your request.'));
+}
+
+export function getTrending(page) {
+  return apiService(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`,
   );
-  return data.hits;
-};
+}
 
-export default apiService;
+export function searchMovies(query, page) {
+  return apiService(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`,
+  );
+}
+
+export function getMovieDetails(movieId) {
+  return apiService(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+}
+
+export function getMovieCredits(movieId) {
+  return apiService(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
+}
+
+export function getMovieReviews(movieId, page) {
+  return apiService(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&page=${page}`,
+  );
+}
