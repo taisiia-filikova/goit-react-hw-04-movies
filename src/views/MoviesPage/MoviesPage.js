03c9f46c17acc63e706264780830e6de47bcf2fc
taisiia-filikova <filikova@gmail.com>
2021-01-23 22:33:42 +0200

    Создает карточки фильмов

---
+++ src/views/MoviesPage/MoviesPage.js
@@ -0,0 +1,127 @@
+import { useState, useEffect } from 'react';
+import { Link, useRouteMatch, useHistory, useLocation } from 'react-router-dom';
+import slugify from 'slugify';
+import { Pagination } from '@material-ui/lab';
+
+import * as apiService from '../../services/apiService';
+import PaginationStyles from '../HomePage/Pagination';
+import Status from '../../services/status';
+import Loader from '../../components/Loader/Loader';
+import ErrorImg from '../../components/ErrorImg/ErrorImg';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import noImg from '../../img/dummy.jpg';
+import s from './MoviesPage.module.css';
+
+const makeSlug = string => slugify(string, { lower: true });
+
+function MoviesPage() {
+  const classes = PaginationStyles();
+  const history = useHistory();
+  const location = useLocation();
+  const { url } = useRouteMatch();
+  const [query, setQuery] = useState('');
+  const [totalPage, setTotalPage] = useState(0);
+  const [movies, setMovies] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+
+  const page = new URLSearchParams(location.search).get('page') ?? 1;
+
+  useEffect(() => {
+    if (location.search === '') {
+      return;
+    }
+
+    const newSearch = new URLSearchParams(location.search).get('query');
+    setQuery(newSearch, page);
+  }, [location.search, page]);
+
+  useEffect(() => {
+    if (!query) return;
+    setStatus(Status.PENDING);
+    apiService
+      .searchMovies(query, page)
+      .then(({ results, total_pages }) => {
+        if (results.length === 0) {
+          setError(`No results were found for ${query}!`);
+          setStatus(Status.REJECTED);
+          return;
+        }
+
+        setMovies(results);
+        setTotalPage(total_pages);
+        setStatus(Status.RESOLVED);
+      })
+      .catch(error => {
+        console.log(error);
+        setError(error.message);
+        setStatus(Status.REJECTED);
+      });
+  }, [query, page]);
+
+  const searchImages = newSearch => {
+    if (query === newSearch) return;
+    setQuery(newSearch);
+    setMovies(null);
+    setError(null);
+    setStatus(Status.IDLE);
+    history.push({ ...location, search: `query=${newSearch}&page=1` });
+  };
+
+  const onHandlePage = (event, page) => {
+    history.push({ ...location, search: `query=${query}&page=${page}` });
+  };
+
+  return (
+    <main className={s.main}>
+      <SearchBar onHandleSubmit={searchImages} />
+
+      {status === Status.PENDING && <Loader />}
+
+      {status === Status.REJECTED && <ErrorImg message={error} />}
+
+      {status === Status.RESOLVED && (
+        <>
+          <ul className={s.moviesList}>
+            {movies.map(movie => (
+              <li key={movie.id} className={s.moviesItem}>
+                <Link
+                  to={{
+                    pathname: `${url}/${makeSlug(
+                      `${movie.title} ${movie.id}`,
+                    )}`,
+                    state: { from: location },
+                  }}
+                >
+                  <img
+                    src={
+                      movie.poster_path
+                        ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                        : noImg
+                    }
+                    alt={movie.title}
+                    className={s.poster}
+                  />
+                </Link>
+                <span className={s.movieTitle}>{movie.title}</span>
+              </li>
+            ))}
+          </ul>
+          {totalPage > 1 && (
+            <Pagination
+              className={classes.root}
+              count={totalPage}
+              onChange={onHandlePage}
+              page={Number(page)}
+              showFirstButton
+              showLastButton
+              size="medium"
+            />
+          )}
+        </>
+      )}
+    </main>
+  );
+}
+
+export default MoviesPage;
