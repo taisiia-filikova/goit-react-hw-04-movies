07c6a3bce1391272c983942f530f846c549b7417
taisiia-filikova <filikova@gmail.com>
2021-01-23 18:48:33 +0200

    Прописывает App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { addBackToTop } from 'vanilla-back-to-top';
+
+import Container from './components/Container/Container';
+import AppBar from './components/AppBar/AppBar';
+import Loader from './components/Loader/Loader';
+
+const HomePage = lazy(() =>
+  import('./views/HomePage/HomePage' /* webpackChunkName: "home-view" */),
+);
+
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage/MoviesPage' /* webpackChunkName: "movies-view" */),
+);
+
+const MovieDetailsPage = lazy(() =>
+  import(
+    './views/MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "movies-details-view" */
+  ),
+);
+
+const NotFoundImg = lazy(() =>
+  import(
+    './views/NotFoundImg/NotFoundImg' /* webpackChunkName: "not-found-view" */
+  ),
+);
 
 function App() {
+  useEffect(() => {
+    addBackToTop({
+      backgroundColor: '#000000',
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar />
+
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
+
+          <Route path="/movies/:slug">
+            <MovieDetailsPage />
+          </Route>
+
+          <Route>
+            <NotFoundImg />
+          </Route>
+        </Switch>
+      </Suspense>
+
+      <ToastContainer autoClose={3700} position="bottom-center" />
+    </Container>
   );
 }
 
